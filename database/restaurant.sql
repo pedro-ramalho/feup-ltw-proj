611e179a0ba05edb5cdd862bc3c1656b870fccf0
Pedro Ramalho <pedrocasramalhocas@gmail.com>
2022-04-09 15:18:22 +0100

    Started working on the database script

---
+++ database/restaurant.sql
@@ -0,0 +1,63 @@
+/*****************
+  Database Script
+******************/
+
+/**************************************************************************
+
+/* USER OF THE WEBSITE */
+/* CAN BE BOTH A Customer OR A Restaurant Owner */
+
+DROP TABLE IF EXISTS User; 
+
+
+/* TYPES OF USERS */
+
+DROP TABLE IF EXISTS RestaurantOwner;
+DROP TABLE IF EXISTS Customer;
+
+
+/* OTHERS */
+
+DROP TABLE IF EXISTS Restaurant;
+DROP TABLE IF EXISTS Review;
+DROP TABLE IF EXISTS Order;
+DROP TABLE IF EXISTS Dish;
+
+/**************************************************************************
+
+/* CREATING TABLES */
+
+CREATE TABLE User
+(
+
+);
+
+CREATE TABLE RestaurantOwner
+(
+
+);
+
+CREATE TABLE Customer
+(
+
+);
+
+CREATE TABLE Restaurant
+(
+
+);
+
+CREATE TABLE Review
+(
+
+);
+
+CREATE TABLE Order 
+(
+
+);
+
+CREATE TABLE Dish
+(
+
+);
