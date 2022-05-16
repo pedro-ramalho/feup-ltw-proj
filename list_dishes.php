d5ff83329ee9724baf80b54321569bce903435ff
Pedro Ramalho <pedrocasramalhocas@gmail.com>
2022-05-16 10:16:31 +0100

    Added the HTML structure of the list dishes page

--- list_dishes.php
+++ list_dishes.php
@@ -35,9 +35,58 @@
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/sidebar.css">
   <link rel="stylesheet" href="css/list-dishes-page.css">
-  <title>Document</title>
+  <title>Dishes</title>
 </head>
 <body>
-  
+  <?php draw_header() ?>
+  <main>
+    <section id="dish-previews">
+      <?php
+        foreach ($dishes as $dish)
+          draw_dish($dish);
+      ?>
+    </section>
+    <aside id="dish-filter">
+      <h1>Choose a filter</h1>
+      <form action="list_dishes.php" method="get">
+        <section id="dish-price">
+          <h1>Price</h1>
+          <div id="price-input-container">
+            <label for="min-price">
+              Min<input id="min-price" class="price-number" type="number" name="min_price" min="0" max="100" value="0">
+            </label>
+            <label for="max-price">
+              Max<input id="max-price" class="price-number" type="number" name="max_price" min="0" max="100" value="100">
+            </label>
+          </div>
+        </section>
+        <section id="dish-category">
+          <h1>Categories</h1>
+          <div id="category-input-container">
+            <label for="fast-food">
+              <input id="fast-food" class="category-checkbox" type="checkbox" name="fast-food" value="Fast-food">Fast-food
+            </label>
+            <label for="premium">
+              <input id="premium" class="category-checkbox" type="checkbox" name="premium" value="Premium">Premium
+            </label>
+            <label for="affordable">
+              <input id="affordable" class="category-checkbox" type="checkbox" name="affordable" value="Affordable">Affordable
+            </label>
+            <label for="sushi">
+              <input id="sushi" class="category-checkbox" type="checkbox" name="sushi" value="Sushi">Sushi
+            </label>
+            <label for="vegan">
+              <input id="vegan" class="category-checkbox" type="checkbox" name="vegan" value="Vegan">Vegan
+            </label>
+            <label for="vegetarian">
+              <input id="vegetarian" class="category-checkbox" type="checkbox" name="vegetarian" value="Vegetarian">Vegetarian
+            </label>
+          </div>
+        </section>
+        <button type="submit">Apply</button>
+      </form>
+    </aside>
+  </main>
+  <?php draw_footer() ?>
 </body>
 </html>
