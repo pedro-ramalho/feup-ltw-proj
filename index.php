eff60303c616b68e12618d1f122d3409bbda2ab1
Pedro Ramalho <pedrocasramalhocas@gmail.com>
2022-05-16 18:54:34 +0100

    Code cleanup on style.css and layout.css

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="stylesheet" href="css/layout.css">
   <link rel="stylesheet" href="css/sidebar.css">
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/pages/main-page.css">
