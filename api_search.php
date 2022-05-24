ed0658fe14d118f24c8bcf1ec31f46fbdcfcac14
Pedro Ramalho <pedrocasramalhocas@gmail.com>
2022-05-24 14:29:25 +0100

    Fixed error in search bar

--- api_search.php
+++ api_search.php
@@ -3,9 +3,9 @@
 
   session_start();
 
-  require_once(__DIR__ . '/../database/connection.php');
-  require_once(__DIR__ . '/../classes/restaurant.class.php');
-  require_once(__DIR__ . '/../classes/dish.class.php');
+  require_once(__DIR__ . '/database/connection.php');
+  require_once(__DIR__ . '/classes/restaurant.class.php');
+  require_once(__DIR__ . '/classes/dish.class.php');
 
   $db = get_db();
 
