260e137559a7cfbd718829e8a90e77895a0d7b99
Pedro Ramalho <pedrocasramalhocas@gmail.com>
2022-06-13 17:37:49 +0100

    Made the sign in and sign up pages responsive

--- pages/sign_in.php
+++ pages/sign_in.php
@@ -15,6 +15,7 @@
   <link rel="stylesheet" href="../css/layout.css">
   <link rel="stylesheet" href="../css/style.css">
   <link rel="stylesheet" href="../css/sidebar.css">
+  <link rel="stylesheet" href="../css/responsive.css">
   <link rel="stylesheet" href="../css/pages/main-page.css">
   <link rel="stylesheet" href="../css/pages/page_sign_in.css">
   <link rel="icon" href="../assets/logo/favicon.png">
